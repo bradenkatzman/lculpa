225c1de6e3cd8f1497be0e83e173b197ba24e01d
bradenkatzman <bradenkatzman@gmail.com>
2015-05-04 16:19:51 -0400

    Commented

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app.config['MONGODB_SETTINGS'] = {
 # Create database connection object
 db = MongoEngine(app)
 
+# Review Object
 class Review(db.Document):
     name = db.StringField(required=True)
     department = db.StringField(required=True)
@@ -23,47 +24,70 @@ class Review(db.Document):
 @app.route("/submitreview", methods=['GET', 'POST'])
 def review():
 	if request.method == 'POST':
+		
+		# grab data from form
 		name = request.form["name"]
 		department = request.form["department"]
 		classes = request.form["classes"]
 		summary = request.form["summary"]
 		workload = request.form["workload"]
+		
+		# create and save new review object
 		review = Review(name=name, department=department, classes=classes,
 			summary=summary, workload=workload)
 		review.save()
+
+		# redirect to completed page
 		return redirect("/completedreview")
+	
+	# if HTTP method is 'GET', return submission form
 	return render_template('home.html', reviews=Review.objects)
 
-
+# home page
 @app.route("/", methods=['GET'])
 def index():
 	return render_template('index.html')
 
 @app.route('/results', methods=['GET'])
 def results():
+	#  cast request to string to parse for search key
 	strrequest = str(request)
-	print strrequest
+	# print strrequest
+	
+	# 47:end is search key through end of string, then split key
+	# to get rid of string past search
 	searchKey, garbage = (strrequest[47:]).split('\'')
+	
+	# replace + with space
 	searchKey = searchKey.replace("+", " ")
+	
+	# search database using searchKey
 	nameresults = Review.objects(name=searchKey)
 	classesresults = Review.objects(classes=searchKey)
 	departmentresults = Review.objects(department=searchKey)
+	
+	# return results.html and cursor objects to loop through and print
 	return render_template('results.html', nameresults=nameresults, 
 		departmentresults=departmentresults, classesresults=classesresults)
 
 @app.route('/goldbush', methods=['GET'])
 def goldbush():
+	# search database for gold professors
 	kohnreviews = Review.objects(name="Shira Kohn")
 	androphyreviews = Review.objects(name="Ron Androphy")
 	gampelreviews = Review.objects(name="Benjamin R. Gampel")
+	
+	# return goldbush.html and cursor objects to gold professors
 	return render_template('goldbush.html', kohnreviews=kohnreviews, 
 		androphyreviews=androphyreviews, gampelreviews=gampelreviews)
 
 @app.route('/silverbush', methods=['GET'])
 def silverbush():
+	# search database for silver professors
 	kalmanofskyreviews = Review.objects(name="Amy Kalmanofsky")
-	return render_template('silverbush.html', kalmanofskyreviews=kalmanofskyreviews
-		)
+	
+	# return silverbush.html and cursor objects to silver professors
+	return render_template('silverbush.html', kalmanofskyreviews=kalmanofskyreviews)
 
 @app.route("/completedreview", methods=['GET'])
 def completed():
